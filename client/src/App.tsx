dd8454d48d3dc72203f917dcb261f5d6645094a0
ohaddahan <ohaddahan@gmail.com>
2022-03-11 20:11:11 +0200

    add solana wallet

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,77 @@
-import React, { useEffect } from "react";
+import React, {useEffect, useMemo} from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import {WalletAdapterNetwork} from '@solana/wallet-adapter-base';
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
+import {clusterApiUrl} from '@solana/web3.js';
+import {ConnectionProvider, WalletProvider} from "@solana/wallet-adapter-react";
+import {WalletModalProvider} from "@solana/wallet-adapter-react-ui";
+
+// Default styles that can be overridden by your app
+
 function App() {
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
+  // Only the wallets you configure here will be compiled into your application, and only the dependencies
+  // of wallets that your users connect to will be loaded.
+  const wallets = useMemo(
+      () => [
+        new PhantomWalletAdapter(),
+        new SlopeWalletAdapter(),
+        new SolflareWalletAdapter(),
+        new TorusWalletAdapter(),
+        new LedgerWalletAdapter(),
+        new SolletWalletAdapter({network}),
+        new SolletExtensionWalletAdapter({network}),
+      ],
+      [network]
+  );
+
   useEffect(() => {
     fetch("/api")
-      .then((res) => res.json())
-      .then((res) => alert(res.message));
+        .then((res) => res.json())
+        .then((res) => alert(res.message));
   });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>
+              <div>
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                  </p>
+                  <a
+                      className="App-link"
+                      href="https://reactjs.org"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                    Learn React
+                  </a>
+                </header>
+              </div>
+            </WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
+      </div>
   );
 }
 
